0cdced773b2559bc29c10c894a7e9f0f9e6e14ac
cloudbasti <92829346+cloudbasti@users.noreply.github.com>
2024-11-18 16:51:35 +0000

    Balkendiagramm mit Umsatz fertig

--- woche2.py
+++ woche2.py
@@ -2,10 +2,44 @@ import pandas as pd
 
 df = pd.read_csv("umsatzdaten_gekuerzt.csv")
 
-# filter out by weekdays
+# add weekdays to dataset (not needed in fact)
 
 df['Datum'] = pd.to_datetime(df['Datum'])
 
 df['Tag'] = df['Datum'].dt.day_name()
 
-print(df)
+# calculate average turnover per weekday
+
+monday = df[ (df['Datum'].dt.weekday == 0)]
+mondayAverage = monday["Umsatz"].mean()
+
+tuesday = df[ (df['Datum'].dt.weekday == 1)]
+tuesdayAverage = tuesday["Umsatz"].mean()
+
+wednesday = df[ (df['Datum'].dt.weekday == 2)]
+wednesdayAverage = wednesday["Umsatz"].mean()
+
+thursday = df[ (df['Datum'].dt.weekday == 3)]
+thursdayAverage = thursday["Umsatz"].mean()
+
+friday = df[ (df['Datum'].dt.weekday == 4)]
+fridayAverage = friday["Umsatz"].mean()
+
+saturday = df[ (df['Datum'].dt.weekday == 5)]
+saturdayAverage = saturday["Umsatz"].mean()
+
+sunday = df[ (df['Datum'].dt.weekday == 6)]
+sundayAverage = sunday["Umsatz"].mean()
+
+# store results in array to pass to matplotlib
+
+
+# graphical visualisation
+import matplotlib.pyplot as plt
+
+Wochentag = ('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag')
+Umsatz = [mondayAverage, tuesdayAverage, wednesdayAverage, thursdayAverage, fridayAverage, saturdayAverage, sundayAverage]
+ 
+plt.bar(Wochentag, Umsatz, align='center')
+
+plt.show()
