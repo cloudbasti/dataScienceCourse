7ab5515cf2d85bb4cb17d9718cde616d8ef9ce76
cloudbasti <92829346+cloudbasti@users.noreply.github.com>
2024-11-18 15:18:49 +0000

    Woche Eins bis auf -signifikante Unterscheidung-

--- woche1.py
+++ woche1.py
@@ -1,5 +1,32 @@
 import pandas as pd
 
-data = pd.read_csv("wetter.csv")
+df = pd.read_csv("wetter.csv")
 
-print(data.head())
+print(df.head())
+
+# extract temperature column and find average temperature
+
+temp = df["Temperatur"]
+
+print(temp.mean())
+
+# average temperature of July and May
+df['Datum'] = pd.to_datetime(df['Datum'])
+
+july = df[ (df['Datum'].dt.month == 7)]
+
+may = df[ (df['Datum'].dt.month == 5)]
+
+julyTemp = july["Temperatur"]
+mayTemp = may["Temperatur"]
+
+julyAverage = julyTemp.mean()
+mayAverage = mayTemp.mean()
+
+print(julyAverage)
+print(mayAverage)
+
+
+
+
+
